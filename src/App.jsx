0c4d66c2bbd903353512a2559afb67fcc47f5475
AkankshaShinde23 <2022.akanksha.shinde@ves.ac.in>
2024-09-10 22:01:09 +0530

    changed the home page

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import NavBar from './components/navbar/NavBar';
+import Footer from './components/footer/Footer';
+import Banner from './components/banner/Banner';
+import Promo from './components/promo/Promo';
+
+function App() {
+    return (
+        <>
+            <NavBar />
+            <Banner/>
+            <Promo/>            
+            <Footer/>
+        </>
+    );
+}
+
+export default App;
---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,16 @@
+import "./Home.css";
+
+export default function Home() {
+    return (
+        <div className="container">
+          <nav>
+          <div className="button-container">
+            <button className="button">Sell</button>
+            <button className="button">Earn</button>
+            <button className="button">Guide</button>
+            <button className="button">Contact</button>
+          </div>
+          </nav>
+        </div>
+      );
+}
